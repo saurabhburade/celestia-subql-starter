7c983faf6a96cfbd87000081c275bd7ed90e4f1f
Saurabh Burade <sburade3@gmail.com>
2025-01-14 13:08:01 +0530

    fix: refactor

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -14,6 +14,12 @@ export async function handleBlock(block: CosmosBlock): Promise<void> {
 export async function handleBlock(block: CosmosBlock): Promise<void> {
   // If you want to index each block in Cosmos (CosmosHub), you could do that here
   logger.info(`BLOCK ::  ${block?.block?.header?.height}`);
+  const txs = block.txs;
+  txs.forEach((tx) => {
+    logger.info(`EVENTS ::  ${JSON.stringify(tx?.events)}`);
+    logger.info(`LOGS ::  ${JSON.stringify(tx?.log)}`);
+    logger.info(`DATA ::  ${JSON.stringify(tx?.data)}`);
+  });
 }
 /*
 export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
