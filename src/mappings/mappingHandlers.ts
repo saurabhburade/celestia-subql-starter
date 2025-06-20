e1519dd5c0d9f1776ef89dbc9016623d1d0e7fdc
saurabhburade <sburade@gmail.com>
2025-06-20 23:54:11 +0530

    fix: avoid concurrent ops

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -276,28 +276,28 @@ export async function handleBlock(block: CosmosBlock): Promise<void> {
   // await store.bulkUpdate("CollectiveDayData", collectiveDayDatas);
   // await store.bulkUpdate("CollectiveHourData", collectiveHourDatas);
   await Promise.all([
-    store.bulkUpdate("CollectiveData", collectiveDataEntities),
-    store.bulkUpdate("CollectiveDayData", collectiveDayDatas),
-    store.bulkUpdate("CollectiveHourData", collectiveHourDatas),
-    store.bulkUpdate("AppEntity", appEntities),
-    store.bulkUpdate("AppDayData", appDayDatas),
-    store.bulkUpdate("AppHourData", appHourDatas),
-    store.bulkUpdate("AccountEntity", accountEntities),
-    store.bulkUpdate("AccountDayData", accountDayDatas),
-    store.bulkUpdate("AccountHourData", accountHourDatas),
+    // store.bulkUpdate("CollectiveData", collectiveDataEntities),
+    // store.bulkUpdate("CollectiveDayData", collectiveDayDatas),
+    // store.bulkUpdate("CollectiveHourData", collectiveHourDatas),
+    // store.bulkUpdate("AppEntity", appEntities),
+    // store.bulkUpdate("AppDayData", appDayDatas),
+    // store.bulkUpdate("AppHourData", appHourDatas),
+    // store.bulkUpdate("AccountEntity", accountEntities),
+    // store.bulkUpdate("AccountDayData", accountDayDatas),
+    // store.bulkUpdate("AccountHourData", accountHourDatas),
     store.bulkUpdate("BlobData", blobs),
     store.bulkUpdate("TransactionData", txnRecords),
     bdata.save(),
   ]);
 
-  // logger.info(`BEFORE BULK UPDATES :: APPS`);
-  // await store.bulkUpdate("AppEntity", appEntities);
-  // await store.bulkUpdate("AppDayData", appDayDatas);
-  // await store.bulkUpdate("AppHourData", appHourDatas);
-  // logger.info(`BEFORE BULK UPDATES :: ACCOUNTS`);
-  // await store.bulkUpdate("AccountEntity", accountEntities);
-  // await store.bulkUpdate("AccountDayData", accountDayDatas);
-  // await store.bulkUpdate("AccountHourData", accountHourDatas);
+  logger.info(`BEFORE BULK UPDATES :: APPS`);
+  await store.bulkUpdate("AppEntity", appEntities);
+  await store.bulkUpdate("AppDayData", appDayDatas);
+  await store.bulkUpdate("AppHourData", appHourDatas);
+  logger.info(`BEFORE BULK UPDATES :: ACCOUNTS`);
+  await store.bulkUpdate("AccountEntity", accountEntities);
+  await store.bulkUpdate("AccountDayData", accountDayDatas);
+  await store.bulkUpdate("AccountHourData", accountHourDatas);
   // logger.info(`BEFORE BULK UPDATES:: BLOBS | TXNS`);
   // await store.bulkUpdate("BlobData", blobs);
   // await store.bulkUpdate("TransactionData", txnRecords);
